4f60e8c8a89e3bfb64901a8bac48247e485ffba7
David Moore <hey@iamdavidmoore.com>
2018-06-13 02:02:43 +0000

    automatically backed up by learn

--- tcdocs/lib/tcdocs.rb
+++ tcdocs/lib/tcdocs.rb
@@ -1,3 +1,6 @@
 # this is the environment file
 require_relative "./tcdocs/version"
 require_relative "./tcdocs/tcdocscontroller"
+require_relative "./scraper"
+require_relative "./documentary"
+require_relative "./category"
